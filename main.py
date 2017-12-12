fb2858e4ff65db4216618ca59fa0bfc626786d35
Alexei Kostevich <alexei.kostevich@live.com>
2017-12-12 05:04:43 +0300

    Refactor main.py

--- main.py
+++ main.py
@@ -1,28 +1,20 @@
 import curses
 import time
-from game_of_life import Life, World
+from game_of_life import Life
 
 
 def main(screen):
+    curses.curs_set(0)
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+
     height, width = screen.getmaxyx()
 
     for world in Life.of_world(width // 2, height):
-        render(world, screen)
+        screen.addstr(0, 0, str(world), curses.color_pair(1))
+        screen.refresh()
         time.sleep(.25)
 
 
-def render(world: World, screen):
-    screen.addstr(0, 0, str(world), curses.color_pair(1))
-    screen.refresh()
-
-
 if __name__ == '__main__':
-    try:
-        screen = curses.initscr()
-        curses.curs_set(0)
-        curses.start_color()
-        curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
-
-        main(screen)
-    except KeyboardInterrupt:
-        curses.endwin()
+    print(curses.wrapper(main))
