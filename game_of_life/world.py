6ef6c3539d75a8daf9f9d8753c390d3273b06f54
Alexei Kostevich <alexei.kostevich@live.com>
2017-12-12 05:10:21 +0300

    Refactor codebase

--- game_of_life/world.py
+++ game_of_life/world.py
@@ -1,9 +1,8 @@
-from typing import Iterable, Tuple
 from .cell import Cell
 from .sparse_grid import InfiniteSparseGrid
 
 
-class World(object):
+class World(InfiniteSparseGrid[Cell]):
     def __init__(self, width: int, height: int):
         if width <= 3:
             raise ValueError('width is less than 3.')
@@ -11,30 +10,7 @@ class World(object):
         if height <= 3:
             raise ValueError('height is less than 3.')
 
-        self._grid = InfiniteSparseGrid(width, height)
-
-    @property
-    def width(self) -> int:
-        return self._grid.width
-
-    @property
-    def height(self) -> int:
-        return self._grid.height
-
-    def get_neibours(self, x: int, y: int) -> Iterable[Cell]:
-        return self._grid.get_neibours(x, y)
-
-    def positions(self) -> Tuple[int, int]:
-        return self._grid.positions()
-
-    def __getitem__(self, position: Tuple[int, int]) -> Cell:
-        return self._grid[position]
-
-    def __setitem__(self, position: Tuple[int, int], value: Cell):
-        self._grid[position] = value
-
-    def __str__(self) -> str:
-        return self._grid.__str__()
+        super().__init__(width, height)
 
     @classmethod
     def random(cls, width: int, height: int):
