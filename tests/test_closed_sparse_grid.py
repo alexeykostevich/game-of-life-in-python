e505cb3cb7fc4fe98cdcc4129aff855e81ba8a90
Alexei Kostevich <alexei.kostevich@live.com>
2017-12-13 03:56:10 +0300

    Add unit tests for 'SparseGridTestCase'

--- tests/test_closed_sparse_grid.py
+++ tests/test_closed_sparse_grid.py
@@ -1,72 +1,72 @@
 import unittest
-from game_of_life.sparse_grid import SparseGrid
+from game_of_life.sparse_grid import ClosedSparseGrid
 
 
-class SparseGridTestCase(unittest.TestCase):
+class ClosedSparseGridTestCase(unittest.TestCase):
     def test_init(self):
         with self.assertRaises(ValueError):
-            SparseGrid(0, 1)
+            ClosedSparseGrid(0, 1)
 
         with self.assertRaises(ValueError):
-            SparseGrid(1, 0)
+            ClosedSparseGrid(1, 0)
 
         with self.assertRaises(ValueError):
-            SparseGrid(-1, 1)
+            ClosedSparseGrid(-1, 1)
 
         with self.assertRaises(ValueError):
-            SparseGrid(1, -1)
+            ClosedSparseGrid(1, -1)
 
-        self.assertIsInstance(SparseGrid(1, 1), SparseGrid)
+        self.assertIsInstance(ClosedSparseGrid(1, 1), ClosedSparseGrid)
 
     def test_width(self):
-        grid = SparseGrid(2, 1)
+        grid = ClosedSparseGrid(2, 1)
 
         self.assertEqual(grid.width, 2)
 
     def test_height(self):
-        grid = SparseGrid(1, 2)
+        grid = ClosedSparseGrid(1, 2)
 
         self.assertEqual(grid.height, 2)
 
     def test_get_positions(self):
-        grid = SparseGrid(2, 2)
+        grid = ClosedSparseGrid(2, 2)
 
         self.assertEqual(list(grid.get_positions()), [(0, 0), (1, 0), (0, 1), (1, 1)])
 
     def test_get_neighbours_positions_for(self):
-        grid = SparseGrid.from_data(
+        grid = ClosedSparseGrid.from_data(
             [1, 2, 3],
             [4, 5, 6],
             [7, 8, 9]
         )
 
         positions = grid.get_neighbours_positions_for(0, 0)
-        self.assertEqual(list(positions), [(1, 0), (1, 1), (0, 1)])
+        self.assertEqual(list(positions), [(-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)])
 
         positions = grid.get_neighbours_positions_for(2, 2)
-        self.assertEqual(list(positions), [(1, 1), (2, 1), (1, 2)])
+        self.assertEqual(list(positions), [(1, 1), (2, 1), (3, 1), (3, 2), (3, 3), (2, 3), (1, 3), (1, 2)])
 
         positions = grid.get_neighbours_positions_for(1, 1)
         self.assertEqual(list(positions), [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2), (1, 2), (0, 2), (0, 1)])
 
     def test_get_neighbours_for(self):
-        grid = SparseGrid.from_data(
+        grid = ClosedSparseGrid.from_data(
             [1, 2, 3],
             [4, 5, 6],
             [7, 8, 9]
         )
 
         neighbours = grid.get_neighbours_for(0, 0)
-        self.assertEqual(list(neighbours), [2, 5, 4])
+        self.assertEqual(list(neighbours), [9, 7, 8, 2, 5, 4, 6, 3])
 
         neighbours = grid.get_neighbours_for(2, 2)
-        self.assertEqual(list(neighbours), [5, 6, 8])
+        self.assertEqual(list(neighbours), [5, 6, 4, 7, 1, 3, 2, 8])
 
         neighbours = grid.get_neighbours_for(1, 1)
         self.assertEqual(list(neighbours), [1, 2, 3, 6, 9, 8, 7, 4])
 
     def test_get_rows(self):
-        grid = SparseGrid.from_data(
+        grid = ClosedSparseGrid.from_data(
             [1, 2],
             [3, None]
         )
@@ -76,7 +76,7 @@ class SparseGridTestCase(unittest.TestCase):
         self.assertEqual(rows, [[1, 2], [3, None]])
 
     def test_get_columns(self):
-        grid = SparseGrid.from_data(
+        grid = ClosedSparseGrid.from_data(
             [1, 3],
             [2, None]
         )
@@ -86,54 +86,36 @@ class SparseGridTestCase(unittest.TestCase):
         self.assertEqual(columns, [[1, 2], [3, None]])
 
     def test_adjust_position(self):
-        grid = SparseGrid(2, 2)
+        grid = ClosedSparseGrid(2, 2)
 
-        with self.assertRaises(IndexError):
-            grid.adjust_position(-1, 0)
-
-        with self.assertRaises(IndexError):
-            grid.adjust_position(2, 0)
-
-        with self.assertRaises(IndexError):
-            grid.adjust_position(0, -1)
-
-        with self.assertRaises(IndexError):
-            grid.adjust_position(0, 2)
-
+        self.assertEqual(grid.adjust_position(-1, -1), (1, 1))
+        self.assertEqual(grid.adjust_position(2, 2), (0, 0))
         self.assertEqual(grid.adjust_position(0, 0), (0, 0))
 
     def test_is_position_in_range(self):
-        grid = SparseGrid(2, 2)
+        grid = ClosedSparseGrid(2, 2)
 
-        self.assertFalse(grid.is_position_in_range(-1, 0))
-        self.assertFalse(grid.is_position_in_range(2, 0))
-        self.assertFalse(grid.is_position_in_range(0, -1))
-        self.assertFalse(grid.is_position_in_range(0, 2))
+        self.assertTrue(grid.is_position_in_range(-1, 0))
+        self.assertTrue(grid.is_position_in_range(2, 0))
+        self.assertTrue(grid.is_position_in_range(0, -1))
+        self.assertTrue(grid.is_position_in_range(0, 2))
         self.assertTrue(grid.is_position_in_range(0, 0))
 
     def test_get_item(self):
-        grid = SparseGrid.from_data(
+        grid = ClosedSparseGrid.from_data(
             [1, 3],
             [2, None]
         )
 
         self.assertEqual(grid[0, 0], 1)
         self.assertEqual(grid[1, 1], None)
-
-        with self.assertRaises(IndexError):
-            grid[-1, 0]
-
-        with self.assertRaises(IndexError):
-            grid[2, 0]
-
-        with self.assertRaises(IndexError):
-            grid[0, -1]
-
-        with self.assertRaises(IndexError):
-            grid[0, 2]
+        self.assertEqual(grid[-1, 0], 3)
+        self.assertEqual(grid[2, 0], 1)
+        self.assertEqual(grid[0, -1], 2)
+        self.assertEqual(grid[0, 2], 1)
 
     def test_set_item(self):
-        grid = SparseGrid(2, 2)
+        grid = ClosedSparseGrid(2, 2)
 
         grid[0, 0] = 1
         self.assertEqual(grid[0, 0], 1)
@@ -141,20 +123,20 @@ class SparseGridTestCase(unittest.TestCase):
         grid[1, 1] = 2
         self.assertEqual(grid[1, 1], 2)
 
-        with self.assertRaises(IndexError):
-            grid[-1, 0] = 3
+        grid[-1, 0] = 3
+        self.assertEqual(grid[1, 0], 3)
 
-        with self.assertRaises(IndexError):
-            grid[2, 0] = 4
+        grid[2, 0] = 4
+        self.assertEqual(grid[0, 0], 4)
 
-        with self.assertRaises(IndexError):
-            grid[0, -1] = 5
+        grid[0, -1] = 5
+        self.assertEqual(grid[0, 1], 5)
 
-        with self.assertRaises(IndexError):
-            grid[0, 2] = 6
+        grid[0, 2] = 6
+        self.assertEqual(grid[0, 0], 6)
 
     def test_str(self):
-        grid = SparseGrid.from_data(
+        grid = ClosedSparseGrid.from_data(
             [None, 3],
             [2, None]
         )
@@ -162,7 +144,7 @@ class SparseGridTestCase(unittest.TestCase):
         self.assertMultiLineEqual(str(grid), '  3\n2  ')
 
     def test_from_data(self):
-        grid = SparseGrid.from_data(
+        grid = ClosedSparseGrid.from_data(
             [1, 2],
             [3, None, 5]
         )
@@ -171,9 +153,7 @@ class SparseGridTestCase(unittest.TestCase):
         self.assertEqual(grid[1, 0], 2)
         self.assertEqual(grid[0, 1], 3)
         self.assertEqual(grid[1, 1], None)
-
-        with self.assertRaises(IndexError):
-            grid[2, 1]
+        self.assertEqual(grid[2, 1], 3)
 
 
 if __name__ == '__main__':
